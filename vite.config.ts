4f0634609e1f9f3b8e14caf8690e0e7a57bb207f
afeiship <1290657123@qq.com>
2022-08-04 21:56:12 +0800

    fix: locales missing

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
 // import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
@@ -12,5 +14,17 @@ export default defineConfig({
     outDir: '../docs',
     emptyOutDir: true
   },
-  plugins: [reactRefresh()]
+  // vite copy public assets
+
+  plugins: [
+    reactRefresh(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: './locales',
+          dest: './locales'
+        }
+      ]
+    })
+  ]
 });
