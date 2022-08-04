f2acee0bf82dd270b7b1973602668cb4bed4dfd7
afeiship <1290657123@qq.com>
2022-08-04 20:27:06 +0800

    feat: optmzie styles

--- public/src/app.tsx
+++ public/src/app.tsx
@@ -23,34 +23,38 @@ export default () => {
   nx.t = t;
   nx.i18n = i18n;
 
+  const img = i18n.language === 'zh-CN' ? '5' : '1';
+
   return (
     <Container>
       <div className='App'>
         <header className='App-header'>
-          <Avatar src='https://randomuser.me/api/portraits/lego/7.jpg' size={120} />
-          <Select
-            value={i18n.language}
-            style={{ width: 120 }}
-            onChange={(value) => i18n.changeLanguage(value)}
-            options={items}
-          />
-          <p dangerouslySetInnerHTML={
-            { __html: t('desc', { interpolation: { escapeValue: false } }) }
-          }>
-          </p>
-          <h1 style={{ color: '#fff' }}>{t('key')}</h1>
-          <br />
           <Space>
-            <Table style={{ width: 400 }} dataSource={[]} />
-            {/* @ts-ignore */}
-            <DatePicker />
-            <TimePicker />
-            <Button type='primary' onClick={() => setVisible(true)}>{t('open-a-modal')}</Button>
+            <Avatar src={`https://randomuser.me/api/portraits/lego/${img}.jpg`} size={120} />
+            <Select
+              value={i18n.language}
+              style={{ width: 120 }}
+              onChange={(value) => i18n.changeLanguage(value)}
+              options={items}
+            />
+          </Space>
+          <Space direction='vertical'>
+            <h1 style={{ color: '#fff' }}>{t('key')}</h1>
+            <p dangerouslySetInnerHTML={
+              { __html: t('desc', { interpolation: { escapeValue: false } }) }
+            } />
+            <Table style={{ width: 600 }} dataSource={[]} />
+            <Space>
+              {/* @ts-ignore */}
+              <DatePicker />
+              <TimePicker />
+              <Button type='primary' onClick={() => setVisible(true)}>{t('open-a-modal')}</Button>
+            </Space>
           </Space>
         </header>
 
         <Modal visible={visible} onCancel={() => setVisible(false)}>
-          From a modal
+          {t('mtxt')}
         </Modal>
       </div>
     </Container>
--- public/src/main.tsx
+++ public/src/main.tsx
@@ -1,12 +1,13 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
 import './style.scss';
 import { init4backend, LocaleProvider } from '../../src/main';
 import LngDetect from './lng-detect';
 import App from './app';
 
-init4backend(LngDetect);
+init4backend(LngDetect, null, {
+  debug: false
+});
 
 ReactDOM.render(
   <Suspense fallback={<div>Loading...</div>}>
