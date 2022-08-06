a82bf304566597c803266bf5d83a05c6199048d4
afeiship <1290657123@qq.com>
2022-08-06 08:19:13 +0800

    fix: rm init mode export

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { useTranslation } from 'react-i18next';
 import i18next from 'i18next';
 import LocaleProvider from './components/locale-provider';
-import init4backend from './components/init4backend';
-import init4memory from './components/init4memory';
 
 export const useIntl = useTranslation;
-export { LocaleProvider, init4backend, init4memory, i18next };
+export { LocaleProvider, i18next };
